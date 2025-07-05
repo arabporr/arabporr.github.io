377615cbd8ff68328a021b612eec6ba463bdb371
arabporr <42333732+arabporr@users.noreply.github.com>
2025-07-05 03:48:07 +0000

    Deploying to gh-pages from @ arabporr/arabporr.github.io@21dcd355f1c7269031566b3b6d9bd60609ac62ef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,28 +501,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%72%61%62%70%6F%75%72@%6D%63%6D%61%73%74%65%72.%63%61", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/arabporr", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/arabporr", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=xYS8ycUAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
